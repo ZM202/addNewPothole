931a3a7e41708c689d1d3f0833324c7d54b6c55d
Zin <bookcase201@gmail.com>
2020-03-23 17:54:37 -0400

    added html,css, and javscript. still need to work on fixing javascript.

---
+++ script.js
@@ -0,0 +1,6 @@
+
+// Have the user enter the new name for the pothole.//
+
+let newPothole = prompt ("What would you like to name the new pothole?");
+document.getElementById('display').innerHTML = "<p class='button'> Thanks, I'll add that to the list.</p>;
+// let baseMessage = prompt ("Thanks, I'll add that to the list!");
